df3b5792b50d34cb8af60887077b3df8b546c7d3
Jan Groth <jan.groth.de@gmail.com>
2017-09-01 17:49:17 +1000

    Create project layout.

---
+++ tests/test_gitcv.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class GitcvTest(unittest.TestCase):
+    def test_should_test_something(self):
+        self.assertEqual(True, True)
+
+if __name__ == '__main__':
+    unittest.main()
