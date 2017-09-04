049075fa7a382a6921a58bb9c80f5dc36bf1f327
Jan Groth <jan.groth.de@gmail.com>
2017-09-04 22:01:05 +1000

    Setup tests

--- gitcv/gitcv.py
+++ gitcv/gitcv.py
@@ -1,1 +1,18 @@
-print("hello")
+import yaml
+
+
+class GitCv:
+    def __init__(self, cv_path):
+        self._cv = self._load_cv(cv_path)
+
+    def _load_cv(self, cv_path):
+        with open(cv_path, "r") as f:
+            cv = yaml.load_all(f)
+        return cv
+
+    def render(self, cv_path):
+        pass
+
+
+if __name__ == '__main__':
+    GitCv('../cv.yaml').render()
--- tests/test_gitcv.py
+++ tests/test_gitcv.py
@@ -1,9 +1,18 @@
 import unittest
 
+from gitcv.gitcv import GitCv
+
 
 class GitcvTest(unittest.TestCase):
-    def test_should_test_something(self):
-        self.assertEqual(True, True)
+    def test_should_read_simple_cv(self):
+        # setup
+        gitcv = GitCv('resources/simple_cv.yaml')
+
+        # exercise
+        cv = gitcv._cv
+
+        self.assertEqual(cv['foo'], 'bar')
+
 
 if __name__ == '__main__':
     unittest.main()
