e5aa9945d0dab11b7511c8c3ad3aff5c0841419f
Jan Groth <jan.groth.de@gmail.com>
2017-09-05 22:32:53 +1000

    Create empty repository

--- gitcv/gitcv.py
+++ gitcv/gitcv.py
@@ -1,18 +1,26 @@
+import os
+
 import yaml
+from git import Repo
 
 
 class GitCv:
-    def __init__(self, cv_path):
+    def __init__(self, cv_path, repo_path):
         self._cv = self._load_cv(cv_path)
+        self._repo_path = repo_path
 
     def _load_cv(self, cv_path):
         with open(cv_path, "r") as f:
             cv = yaml.load(f)
         return cv
 
-    def render(self, cv_path):
-        pass
+    def _create_repo(self):
+        repo_path = os.path.join(self._repo_path, 'cv')
+        Repo.init(repo_path)
+
+    def render(self):
+        self._create_repo()
 
 
 if __name__ == '__main__':
-    GitCv('../cv.yaml').render()
+    GitCv('../cv.yaml', '../target').render()
--- tests/test_gitcv.py
+++ tests/test_gitcv.py
@@ -1,18 +1,34 @@
+import os
+import shutil
+import tempfile
 import unittest
 
 from gitcv.gitcv import GitCv
 
 
 class GitcvTest(unittest.TestCase):
-    def test_should_read_simple_cv(self):
-        # setup
-        gitcv = GitCv('resources/simple_cv.yaml')
 
+    def setUp(self):
+        self.tmp_path = tempfile.mkdtemp('', 'gitcv_test')
+        self.gitcv = GitCv('resources/simple_cv.yaml', self.tmp_path)
+
+    def tearDown(self):
+        shutil.rmtree(self.tmp_path)
+
+    def test_should_read_simple_cv(self):
         # exercise
-        cv = gitcv._cv
+        cv = self.gitcv._cv
 
         # verify
         self.assertEqual(cv['streams'][0]['edu'][0]['year'], 2001)
+
+    def test_should_create_empty_repo(self):
+        # exercise
+        self.gitcv.render()
+
+        # verify
+        git_repo = os.listdir(self.tmp_path)[0]
+        self.assertEqual(git_repo, 'cv')
 
 
 if __name__ == '__main__':
