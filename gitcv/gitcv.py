66e553f4c61528211b0ebd2b1c7b77cd476c25fa
Jan Groth <jan.groth.de@gmail.com>
2017-09-10 19:12:36 +1000

    Create branch from cv stream.

--- gitcv/gitcv.py
+++ gitcv/gitcv.py
@@ -8,6 +8,7 @@ class GitCv:
     def __init__(self, cv_path, repo_path):
         self._cv = self._load_cv(cv_path)
         self._repo_path = repo_path
+        self._repo_name = 'cv'
 
     def _load_cv(self, cv_path):
         with open(cv_path, "r") as f:
@@ -15,12 +16,27 @@ class GitCv:
         return cv
 
     def _create_repo(self):
-        repo_path = os.path.join(self._repo_path, 'cv')
-        Repo.init(repo_path)
+        repo_path = os.path.join(self._repo_path, self._repo_name)
+        self._repo = Repo.init(repo_path)
 
-    def render(self):
+    def _create_branches(self):
+        for stream in self._cv:
+            for entry in stream:
+                self._create_branch(entry)
+
+    def _create_branch(self, branch_name):
+        self._repo.create_head(branch_name)
+
+    def _create_file_and_commit(self, file_name):
+        open(os.path.join(self._repo_path, self._repo_name, file_name), 'wb').close()
+        self._repo.index.add([file_name])
+        self._repo.index.commit('Add {0}'.format(file_name))
+
+    def create(self):
         self._create_repo()
+        self._create_file_and_commit('dummy.txt')
+        self._create_branches()
 
 
 if __name__ == '__main__':
-    GitCv('../cv.yaml', '../target').render()
+    GitCv('../cv.yaml', '../target').create()
--- tests/test_gitcv.py
+++ tests/test_gitcv.py
@@ -2,37 +2,50 @@ import os
 import shutil
 import tempfile
 import unittest
+from git import Repo
 
 from gitcv.gitcv import GitCv
 
 
 class GitcvTest(unittest.TestCase):
-
     def setUp(self):
-        self.tmp_path = tempfile.mkdtemp('', 'gitcv_test')
+        self.repo_path = tempfile.mkdtemp('', 'gitcv_test')
         resource_path = self._get_abs_path('resources/simple_cv.yaml')
-        self.gitcv = GitCv(resource_path, self.tmp_path)
+        self.gitcv = GitCv(resource_path, self.repo_path)
+        print("Create repo at {}".format(self.repo_path))
 
     def tearDown(self):
-        shutil.rmtree(self.tmp_path)
+        shutil.rmtree(self.repo_path)
 
     def test_should_read_simple_cv(self):
         # exercise
         cv = self.gitcv._cv
 
         # verify
-        self.assertEqual(cv['streams'][0]['edu'][0]['year'], 2001)
+        self.assertEqual(cv[0]['education'][0]['year'], 2001)
 
     def test_should_create_empty_repo(self):
         # exercise
-        self.gitcv.render()
+        self.gitcv.create()
 
         # verify
-        git_repo = os.listdir(self.tmp_path)[0]
+        git_repo = os.listdir(self.repo_path)[0]
         self.assertEqual(git_repo, 'cv')
+
+    def test_should_create_branch_per_stream(self):
+        # exercise
+        self.gitcv.create()
+
+        # verify
+        repo = self._get_repo()
+        self.assertEqual(len(repo.branches), 2)
+        self.assertTrue('education' in repo.branches)
 
     def _get_abs_path(self, rel_path):
         return os.path.join((os.path.dirname(os.path.realpath(__file__))), rel_path)
+
+    def _get_repo(self):
+        return Repo(os.path.join(self.repo_path, 'cv'))
 
 
 if __name__ == '__main__':
